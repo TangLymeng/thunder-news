f96bc27318521d865dd7e89914446acf195aee1c
YuErMeng <tanglymeng2@gmail.com>
2023-05-11 20:35:33 +0700

    [TASK] Setup Login, logout, error message and admin guard

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Admin;
+use App\Mail\Websiteemail;
 use Hash;
 use Auth;
 
@@ -12,12 +13,34 @@ class AdminLoginController extends Controller
 {
     public function index()
     {
-        $pass = Hash::make('123456789');
         return view('admin.login');
     }
 
     public function forget_password()
     {
         return view('admin.forget_password');
+    }
+    public function login_submit(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6|max:20',
+        ]);
+
+        $credential = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if(Auth::guard('admin')->attempt($credential)){
+            return redirect()->route('admin_home');
+        } else {
+            return redirect()->route('admin_login')->with('error', 'Information is not correct!');
+        }
+    }
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin_login');
     }
 }
