404961a26a3c60d9393d4ccd820807b94da427ea
YuErMeng <tanglymeng2@gmail.com>
2023-05-15 13:08:16 +0700

    [TASK] add image intervention to resize ads images

--- app/Http/Controllers/Admin/AdminAdvertisementController.php
+++ app/Http/Controllers/Admin/AdminAdvertisementController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\HomeAdvertisement;
+use Image;
 
 class AdminAdvertisementController extends Controller
 {
@@ -17,30 +18,34 @@ class AdminAdvertisementController extends Controller
     {
         $home_ad_data = HomeAdvertisement::where('id', 1)->first();
 
-        if($request->hasFile('above_search_ad')) {
+        if ($request->hasFile('above_search_ad')) {
             $request->validate([
                 'above_search_ad' => 'image|mimes:jpg,jpeg,png,gif'
             ]);
 
-            unlink(public_path('uploads/'.$home_ad_data->above_search_ad));
+            unlink(public_path('uploads/' . $home_ad_data->above_search_ad));
 
             $ext = $request->file('above_search_ad')->extension();
-            $final_name = 'above_search_ad'.'.'.$ext;
-            $request->file('above_search_ad')->move(public_path('uploads/'),$final_name);
+            $image = Image::make($request->file('above_search_ad'));
+            $image->resize(1170, 100);
+            $final_name = 'above_search_ad' . '.' . $ext;
+            $image->save(public_path('uploads/' . $final_name));
 
             $home_ad_data->above_search_ad = $final_name;
         }
 
-        if($request->hasFile('above_footer_ad')) {
+        if ($request->hasFile('above_footer_ad')) {
             $request->validate([
                 'above_footer_ad' => 'image|mimes:jpg,jpeg,png,gif'
             ]);
 
-            unlink(public_path('uploads/'.$home_ad_data->above_footer_ad));
+            unlink(public_path('uploads/' . $home_ad_data->above_footer_ad));
 
             $ext = $request->file('above_footer_ad')->extension();
-            $final_name = 'above_footer_ad'.'.'.$ext;
-            $request->file('above_footer_ad')->move(public_path('uploads/'),$final_name);
+            $image = Image::make($request->file('above_footer_ad'));
+            $image->resize(1170, 100);
+            $final_name = 'above_footer_ad' . '.' . $ext;
+            $image->save(public_path('uploads/' . $final_name));
 
             $home_ad_data->above_footer_ad = $final_name;
         }
